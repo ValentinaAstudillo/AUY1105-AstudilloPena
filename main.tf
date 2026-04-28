b3ff1069521b243e29cf9603fe7247728ea493f8
Valentina <v.astudillo@duocuc.cl>
2026-04-28 12:39:00 -0400

    test: validar fallo de politica OPA (SSH publico)

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "aws_security_group" "sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.allowed_ssh_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
