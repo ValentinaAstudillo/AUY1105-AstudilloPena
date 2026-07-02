8f5bdc0223af52191146c1581b800cc40a42acb5
Valentina <v.astudillo@duocuc.cl>
2026-07-02 13:22:45 -0400

    git commit -m "docs: mejorar documentación del módulo computo"

--- modules/computo/main.tf
+++ modules/computo/main.tf
@@ -1,3 +1,4 @@
+# AMI Ubuntu más reciente
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"]
@@ -8,6 +9,7 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+# Instancia EC2
 resource "aws_instance" "ec2" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
